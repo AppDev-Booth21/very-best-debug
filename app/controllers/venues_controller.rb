bc63c71e9d5ebe7573de7aa1723e16e78d7e230c
jweinheibooth <jweinhei@chicagobooth.edu>
2021-08-11 02:11:15 +0000

    85.2%

--- app/controllers/venues_controller.rb
+++ app/controllers/venues_controller.rb
@@ -8,27 +8,38 @@ class VenuesController < ApplicationController
   end
 
   def show
-    venue_id = params.fetch("venue_id")
-    matching_venues = Venue.where({ :id => venue_id })
-    the_venue = matching_venues
+    venue_id = params.fetch("an_id")
+    #matching_venues = Venue.where({ :id => venue_id })
+    matching_venues = Venue.where({ :id => venue_id })    
+    @the_venue = matching_venues.at(0)
 
     render({ :template => "venue_templates/details.html.erb" })
   end
 
   def create
+    # query_address = params.fetch("query_address")
+    # query_name = params.fetch("query_name")
+    # query_neighborhood = params.fetch("query_neighborhood")
+    # venue = Venue.new
+    # venue.address = query_address
+    # venue.name = query_name
+    # venue.neighborhood = query_neighborhood
+    # venue.save
+
     venue = Venue.new
     venue.address = params.fetch("query_address")
     venue.name = params.fetch("query_name")
     venue.neighborhood = params.fetch("query_neighborhood")
     venue.save
 
-    redirect_to("/venues/#{venue.name}")
+    #redirect_to("/venues/#{venue.name}")
+    redirect_to("/venues/#{@the_venue.id}")
   end
   
   def update
-    the_id = params.fetch("venue_id")
-
-    @venue = Venue.where({ :id => the_id })
+    the_id = params.fetch("the_id")
+    matching_venue = Venue.where({ :id => the_id })
+    venue = matching_venue.at(0)
     venue.address = params.fetch("query_address")
     venue.name = params.fetch("query_name")
     venue.neighborhood = params.fetch("query_neighborhood")
@@ -38,9 +49,11 @@ class VenuesController < ApplicationController
   end
 
   def destroy
-    the_id = params.fetch("venue_id")
+
+    #Parameters: {"id_to_delete"=>"43"}
+    the_id = params.fetch("id_to_delete")
     matching_venues = Venue.where({ :id => the_id })
-    venue = matching_venues
+    venue = matching_venues.at(0)
     venue.destroy
 
     redirect_to("/venues")
